ed7598382d3826ab73f00df4abf86ed78b7880fd
vivek srivastava <viveksrivastava3131@gamil.com>
2024-05-25 10:05:12 +0530

    name

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   crypto: {
-    secret: process.env.secret,
+    secret: "mysecretkey",
   },
   touchAfter: 24 * 3600,
 });
