59e20c031f81e729f6ba7aafc5696aed7de278d3
vivek3134 <viveksrivastava3131@gmail.com>
2025-05-17 19:05:54 +0530

    hello

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   // res.locals.currUser = req.user;
-  res.locals.currUser = false;
+  res.locals.currUser = 1;
   // console.log(req.user);    
   next(); 
 });
